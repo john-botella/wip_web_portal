5986605950dca037b63c8a31135c66536cb8e7d2
John BOTELLA <john.botella@atm-consulting.fr>
2023-07-31 09:58:55 +0200

    last fix

--- demo/login.php
+++ demo/login.php
@@ -7,12 +7,23 @@
 	<title>Demo</title>
 	<link rel="stylesheet" href="../css/global.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
+	<style>
+		:root {
+            /* comment this var to remove image and see adaptative linear background */
+            /*--login-background : rgba(0,0,0,0.4) url("https://images.unsplash.com/photo-1552526408-fa252623b415?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDEyNzJ8MHwxfHNlYXJjaHwxNnx8cHVycGxlJTIwZmxvd2VyfGVufDB8MHx8fDE2ODc5NDY5NjB8MA&ixlib=rb-4.0.3&q=80&w=1080&w=1920");*/
+            --login-background: rgba(0, 0, 0, 0.4) url("https://images.unsplash.com/photo-1609607224685-44a36db58b10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1080&w=1920");
+            --login-logo: url("./dolibarr_logo.svg");
+        }
+	</style>
 </head>
 <body class="login-page">
 <div class="login-page__container">
 	<div class="login-screen">
 		<div class="login-screen__content">
 			<form class="login">
+
+				<div class="login__logo" ></div>
+
 				<div class="login__field">
 					<i class="login__icon fas fa-user"></i>
 					<input type="text" class="login__input" placeholder="User name / Email">
