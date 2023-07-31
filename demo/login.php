4e4d39eeb7c00a7a2a7033ac0fbb7d0969a2d799
John BOTELLA <john.botella@atm-consulting.fr>
2023-07-31 11:43:09 +0200

    Fix url

--- demo/login.php
+++ demo/login.php
@@ -8,11 +8,11 @@
 	<link rel="stylesheet" href="../css/global.css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css">
 	<style>
-		:root {
+		.login-page {
             /* comment this var to remove image and see adaptative linear background */
             /*--login-background : rgba(0,0,0,0.4) url("https://images.unsplash.com/photo-1552526408-fa252623b415?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0MDEyNzJ8MHwxfHNlYXJjaHwxNnx8cHVycGxlJTIwZmxvd2VyfGVufDB8MHx8fDE2ODc5NDY5NjB8MA&ixlib=rb-4.0.3&q=80&w=1080&w=1920");*/
             --login-background: rgba(0, 0, 0, 0.4) url("https://images.unsplash.com/photo-1609607224685-44a36db58b10?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1080&w=1920");
-            --login-logo: url("./dolibarr_logo.svg");
+            --login-logo: url("../demo/dolibarr_logo.svg");/* for relative path, must be relative od the css file or use full url starting by http:// */
         }
 	</style>
 </head>
